d696c1f8116e56cbb3dded31be8f286ff5db8980
Giovanny franco <giovannyfranco@mattelsa.net>
2021-05-10 17:23:36 -0500

    Change index of page

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -69,9 +69,9 @@ AppAsset::register($this);
 
 <footer class="footer">
     <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+        <p class="pull-left">&copy; EC Transportes <?= date('Y') ?></p>
 
-        <p class="pull-right"><?= Yii::powered() ?></p>
+        <p class="pull-right"><?= "Creado por <a href='https://github.com/gifrancohe' target='_blank'>@gifrancohe</a>" ?></p>
     </div>
 </footer>
 
