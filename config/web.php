56412696972557e053c8f8e3b553b9f5e71e1cb1
Giovanny franco <giovannyfranco@mattelsa.net>
2021-05-09 19:25:18 -0500

    Fix bug on web config file

--- config/web.php
+++ config/web.php
@@ -5,7 +5,6 @@ $db = require __DIR__ . '/db.php';
 
 $config = [
     'id' => 'ECTrans',
-    'name' => 'EC Trans dashboard',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'aliases' => [
