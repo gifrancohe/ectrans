6a785e27aaa237d9810ec769e684649d50901e3c
Giovanny franco <giovannyfranco@mattelsa.net>
2021-05-10 14:50:13 -0500

    Create new menus

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -39,11 +39,13 @@ AppAsset::register($this);
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
             ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
+            ['label' => 'Cliente', 'url' => ['/site/about']],
+            ['label' => 'Conductor', 'url' => ['/site/contact']],
             Yii::$app->user->isGuest ? (
                 ['label' => 'Login', 'url' => ['/site/login']]
-            ) : (
+            ) : 
+            ['label' => 'Planillas', 'url' => ['/site/contact']],
+            (
                 '<li>'
                 . Html::beginForm(['/site/logout'], 'post')
                 . Html::submitButton(
