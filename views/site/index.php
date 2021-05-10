974f9aacc07374bd75d72bc4ffba5f8d8787622d
Giovanny franco <giovannyfranco@mattelsa.net>
2021-05-10 16:15:42 -0500

    Change index of page

--- views/site/index.php
+++ views/site/index.php
@@ -11,29 +11,31 @@ $this->title = 'EC Transportes';
 
         <p class="lead">Administración de viajes y conductores.</p>
 
-        <p><a class="btn btn-lg btn-success" href="https://ectransportes.com/">Sitio Web</a></p>
+        <p><a class="btn btn-lg btn-success" target="_blank" href="https://ectransportes.com/">Sitio Web</a></p>
     </div>
 
     <div class="body-content">
 
         <div class="row">
             <div class="col-lg-4">
-                <h2>Traslados VIP</h2>
+                <h2>Servicios</h2>
 
-                <p>Contamos con vehículos de lujo marca BMW, Mercedes Benz y Toyota porque entendemos que la seguridad y comodidad de nuestros clienteses primordial.</p>
+                <p>Contamos con vehículos de lujo marca BMW, Mercedes Benz y Toyota porque entendemos que la seguridad y comodidad de nuestros clientes es primordial.</p>
                 <p>Nos enfocamos en dar un servicio preferencial que cubre con excelencia todas tus necesidades.</p>
 
-                <p><a class="btn btn-default" href="https://ectransportes.com/#services">Servicios &raquo;</a></p>
+                <p><a class="btn btn-default" target="_blank" href="https://ectransportes.com/#services">Servicios &raquo;</a></p>
             </div>
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2>Características</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p>Capacidad para movilizar desde 1 hasta 45 pasajeros.</p>
+                <p>Conductores bilingües privados y certificados.</p>
+                <p>Presentación personal impecable.</p>
+                <p>Puntualidad garantizada.</p>
+                <p>Sistema de monitoreo satelital de toda la flota.</p>
+                <p>Reservas ágiles y personalizadas.</p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+                <p><a class="btn btn-default" target="_blank" href="https://ectransportes.com/#caracteristicas">Características &raquo;</a></p>
             </div>
             <div class="col-lg-4">
                 <h2>Heading</h2>
