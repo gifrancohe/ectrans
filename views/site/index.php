ab2a6509106da9d18d15c1452775d2e902b481c3
Giovanny franco <giovannyfranco@mattelsa.net>
2021-05-10 16:05:23 -0500

    Change index of page

--- views/site/index.php
+++ views/site/index.php
@@ -2,30 +2,28 @@
 
 /* @var $this yii\web\View */
 
-$this->title = 'EC Transportes dashboard';
+$this->title = 'EC Transportes';
 ?>
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations!</h1>
+        <h1>EC Transportes</h1>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+        <p class="lead">Administración de viajes y conductores.</p>
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+        <p><a class="btn btn-lg btn-success" href="https://ectransportes.com/">Sitio Web</a></p>
     </div>
 
     <div class="body-content">
 
         <div class="row">
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2>Traslados VIP</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p>Contamos con vehículos de lujo marca BMW, Mercedes Benz y Toyota porque entendemos que la seguridad y comodidad de nuestros clienteses primordial.</p>
+                <p>Nos enfocamos en dar un servicio preferencial que cubre con excelencia todas tus necesidades.</p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+                <p><a class="btn btn-default" href="https://ectransportes.com/#services">Servicios &raquo;</a></p>
             </div>
             <div class="col-lg-4">
                 <h2>Heading</h2>
