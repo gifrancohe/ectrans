b8ad03da5b8760e33881747876eb710f4254bef3
Giovanny franco <giovannyfranco@mattelsa.net>
2021-05-10 20:53:44 -0500

    Form with two columns

--- views/payroll/create.php
+++ views/payroll/create.php
@@ -5,7 +5,7 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\Payroll */
 
-$this->title = Yii::t('app', 'Create Payroll');
+$this->title = Yii::t('app', 'Crear Planilla');
 $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Payrolls'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
