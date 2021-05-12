39fa2b0d0941e51fadb0e1bc4843f806ebcef85d
Giovanny franco <giovannyfranco@mattelsa.net>
2021-05-12 18:01:19 -0500

    Fix instance of picker

--- views/payroll/_form.php
+++ views/payroll/_form.php
@@ -2,7 +2,7 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
-use kartik\widgets\TimePicker;
+use kartik\time\TimePicker;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Payroll */
