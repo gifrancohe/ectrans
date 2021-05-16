4455fb2b62b7d3efeeace3652f1b1f73af64fdd0
Giovanny franco <giovannyfranco@mattelsa.net>
2021-05-15 19:51:34 -0500

    Function to less 5 hour to date on detalview

--- views/driver/_form.php
+++ views/driver/_form.php
@@ -26,9 +26,13 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'created_at')->hiddenInput(['value'=> date('Y-m-d H:i:s')])->label(false); ?>
-
-    <?= $form->field($model, 'updated_at')->hiddenInput(['value'=> date('Y-m-d H:i:s')])->label(false); ?>
+    <?php if($model->isNewRecord): ?>
+          
+        <?= $form->field($model, 'created_at')->hiddenInput(['value'=> date('Y-m-d H:i:s')])->label(false); ?>
+  
+        <?= $form->field($model, 'updated_at')->hiddenInput(['value'=> date('Y-m-d H:i:s')])->label(false); ?>
+      
+    <?php endif; ?>
 
     <div class="form-group">
         <?= Html::submitButton(Yii::t('app', 'Guardar'), ['class' => 'btn btn-success']) ?>
