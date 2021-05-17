af3ae4a09730d0538b7ec82f3bc4cc9d9380ec71
Giovanny franco <giovannyfranco@mattelsa.net>
2021-05-17 10:56:35 -0500

    Add image of background and gradient

--- views/site/index.php
+++ views/site/index.php
@@ -21,12 +21,12 @@ $this->title = 'EC Transportes';
             <?= Carousel::widget([
                 'items' => [
                     // the item contains only the image
-                    '<img src="http://twitter.github.io/bootstrap/assets/img/bootstrap-mdo-sfmoma-01.jpg"/>',
+                    '<img src="../web/ec-background-drivers.jpeg"/>',
                     // equivalent to the above
-                    ['content' => '<img src="http://twitter.github.io/bootstrap/assets/img/bootstrap-mdo-sfmoma-02.jpg"/>'],
+                    ['content' => '<img src="../web/ec-background-drivers.jpeg"/>'],
                     // the item contains both the image and the caption
                     [
-                        'content' => '<img src="http://twitter.github.io/bootstrap/assets/img/bootstrap-mdo-sfmoma-03.jpg"/>',
+                        'content' => '<img src="../web/ec-background-drivers.jpeg"/>',
                         'caption' => '<h4>This is title</h4><p>This is the caption text</p>',
                     ],
                 ]
