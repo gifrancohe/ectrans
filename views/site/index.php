e0f4f59e84ba488f8d60ced93b6d9bee920e67dd
Giovanny franco <giovannyfranco@mattelsa.net>
2021-05-17 11:37:55 -0500

    Add image of background and gradient

--- views/site/index.php
+++ views/site/index.php
@@ -64,7 +64,7 @@ $this->title = 'EC Transportes';
                 <p>Contamos con vehículos de lujo marca BMW, Mercedes Benz y Toyota porque entendemos que la seguridad y comodidad de nuestros clientes es primordial.</p>
                 <p>Nos enfocamos en dar un servicio preferencial que cubre con excelencia todas tus necesidades.</p>
 
-                <p><a class="btn btn-default" target="_blank" href="https://ectransportes.com/#services">Servicios &raquo;</a></p>
+                <p><a class="btn btn-default btn-home" target="_blank" href="https://ectransportes.com/#services">Servicios &raquo;</a></p>
             </div>
             <div class="col-lg-4">
                 <h2>Características</h2>
@@ -76,7 +76,7 @@ $this->title = 'EC Transportes';
                 <p>Sistema de monitoreo satelital de toda la flota.</p>
                 <p>Reservas ágiles y personalizadas.</p>
 
-                <p><a class="btn btn-default" target="_blank" href="https://ectransportes.com/#caracteristicas">Características &raquo;</a></p>
+                <p><a class="btn btn-default btn-home" target="_blank" href="https://ectransportes.com/#caracteristicas">Características &raquo;</a></p>
             </div>
             <div class="col-lg-4">
                 <h2>Transporte</h2>
@@ -84,7 +84,7 @@ $this->title = 'EC Transportes';
                 <p>Somos expertos en transporte de pasajeros en Medellín, Colombia. Nos destacamos por prestar un servicio de alto nivel.</p>
                 <p>Somos expertos en transporte de calidad en Medellín, Colombia. Nos destacamos por prestar un servicio de alto nivel.</p>
 
-                <p><a class="btn btn-default" target="_blank" href="https://ectransportes.com/">Transporte &raquo;</a></p>
+                <p><a class="btn btn-default btn-home" target="_blank" href="https://ectransportes.com/">Transporte &raquo;</a></p>
             </div>
         </div>
 
