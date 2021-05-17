3b97f8724761582863af992b2b777818908278ad
Giovanny franco <giovannyfranco@mattelsa.net>
2021-05-17 10:52:15 -0500

    Add image of background and gradient

--- views/site/index.php
+++ views/site/index.php
@@ -27,7 +27,6 @@ $this->title = 'EC Transportes';
                     [
                         'content' => '<img src="http://twitter.github.io/bootstrap/assets/img/bootstrap-mdo-sfmoma-03.jpg"/>',
                         'caption' => '<h4>This is title</h4><p>This is the caption text</p>',
-                        'options' => [...],
                     ],
                 ]
             ]);
