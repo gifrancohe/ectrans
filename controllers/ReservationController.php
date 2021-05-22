4b8b23bcb7f0d4fda75d45d0022491a37a44d43e
Giovanny franco <giovannyfranco@mattelsa.net>
2021-05-22 11:39:34 -0500

    Add access control to actions

--- controllers/ReservationController.php
+++ controllers/ReservationController.php
@@ -29,6 +29,18 @@ class ReservationController extends Controller
                     'delete' => ['POST'],
                 ],
             ],
+            'access' => [
+                'class' => \yii\filters\AccessControl::className(),
+                'only' => ['index','create','update','view'],
+                'rules' => [
+                    // allow authenticated users
+                    [
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                    // everything else is denied
+                ],
+            ],
         ];
     }
 
